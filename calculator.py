9465d0a434039e9e52879d60c41b3777b00d72a4
djmolletones <dannymolletones@hotmail.com>
2016-10-05 17:34:41 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,27 +6,24 @@ Date: 05/10/2016
 
 
 class CalculatorClass(object):
-    """
-    Clases que define un método que permite obtener
-	la sumatoria de una lista de numeros
-    """
-
+"""
+Clase que define un método que permite obtenerla sumatoria de una lista de numeros
+"""
     def sum(self, num_list):
         """
-        Suma el contenido de la lista num_list que es recibida como 
-	parametro y muestra el resultado por pantalla
+        Suma el contenido de la lista num_list que es recibida como
+        parametro y muestra el resultado por pantalla
         """
-        # your sum code here
         suma = 0
         for i in num_list:
-            suma = suma + i
+                suma = suma + i
         print('La sumatoria es:' + str(suma))
 
 lista = []
 sumatoria = CalculatorClass()
 cantidad_lista = int(input('Indique la cantidad de números a sumar: '))
 for j in range(1, cantidad_lista+1):
-    numero = int(input('indique el valor a sumar: '))
-    lista.append(numero)
+        numero = int(input('indique el valor a sumar: '))
+        lista.append(numero)
 	
 sumatoria.sum(lista)
