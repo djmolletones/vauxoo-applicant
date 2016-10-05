676d08556b3b59aad27b2841198d60d5cc5de791
djmolletones <dannymolletones@hotmail.com>
2016-10-05 17:04:34 -0400

    Update employee_vauxoo.sql

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -8,25 +8,21 @@ CREATE TABLE employee (
   last_name varchar(80),
   PRIMARY KEY (id)
 );
-
 CREATE TABLE employee_department (
   id int,
   name varchar(80),
   description varchar(200),
   PRIMARY KEY (id)
 );
-
 CREATE TABLE employee_hobby (
   id int,
   name varchar(80),
   description varchar(200)
 );
-
 INSERT INTO employee VALUES (1, 'Juan', 'Santiago');
 INSERT INTO employee VALUES (2, 'Mariana', 'Lozada');
 INSERT INTO employee VALUES (3, 'Daniel', 'Ruiz');
 INSERT INTO employee VALUES (4, 'Angela', 'Marin');
-
 INSERT INTO employee_department VALUES (1, 'Finanzas', 'Control de gastos de la empresa');
 INSERT INTO employee_department VALUES (2, 'Mantenimiento', 'Vela por el cuidado de las instalaciones');
 INSERT INTO employee_department VALUES (3, 'Informatica', 'Soporte a todos los sistemas de Computo de la empresa');
@@ -35,11 +31,10 @@ INSERT INTO employee_department VALUES (5, 'Seguridad', 'Vela por la seguridad d
 INSERT INTO employee_department VALUES (6, 'Legal', 'Encargado de todos los asuntos legales de la empresa');
 
 ALTER TABLE employee ADD id_department int;
-ALTER TABLE employee ADD FOREIGN KEY(id_department) REFERENCES employee_department(id)
+ALTER TABLE employee ADD FOREIGN KEY(id_department) REFERENCES employee_department(id);
 
 UPDATE employee SET id_department = 3 WHERE id = 1;
 UPDATE employee SET id_department = 3 WHERE id = 2;
 UPDATE employee SET id_department = 6 WHERE id = 3;
 UPDATE employee SET id_department = 2 WHERE id = 4;
-
 -- ...
