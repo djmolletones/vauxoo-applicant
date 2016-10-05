336b6b40cf18b8fdda0c4bf88340dd92e1b88427
djmolletones <dannymolletones@hotmail.com>
2016-10-05 16:57:29 -0400

    Update employee_vauxoo.sql

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -2,45 +2,44 @@
 -- NOTE: Please, don't add sentence to create database in this script file.
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
-
 CREATE TABLE employee (
-  id serial,
+  id int,
   first_name varchar(80),
   last_name varchar(80),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employee_department (
-  id serial,
+  id int,
   name varchar(80),
   description varchar(200),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employee_hobby (
-  id serial,
+  id int,
   name varchar(80),
   description varchar(200)
 );
 
-INSERT INTO employee (first_name, last_name) VALUES ('Juan', 'Santiago');
-INSERT INTO employee (first_name, last_name) VALUES ('Mariana', 'Lozada');
-INSERT INTO employee (first_name, last_name) VALUES ('Daniel', 'Ruiz');
-INSERT INTO employee (first_name, last_name) VALUES ('Angela', 'Marin');
+INSERT INTO employee VALUES (1, 'Juan', 'Santiago');
+INSERT INTO employee VALUES (2, 'Mariana', 'Lozada');
+INSERT INTO employee VALUES (3, 'Daniel', 'Ruiz');
+INSERT INTO employee VALUES (4, 'Angela', 'Marin');
 
-INSERT INTO employee_department (name, description) VALUES ('Finanzas', 'Control de gastos de la empresa');
-INSERT INTO employee_department (name, description) VALUES ('Mantenimiento', 'Vela por el cuidado de las instalaciones');
-INSERT INTO employee_department (name, description) VALUES ('Informatica', 'Soporte a todos los sistemas de Computo de la empresa');
-INSERT INTO employee_department (name, description) VALUES ('Personal', 'Gestión del Recurso Humano');
-INSERT INTO employee_department (name, description) VALUES ('Seguridad', 'Vela por la seguridad de los bienes y personal');
-INSERT INTO employee_department (name, description) VALUES ('Legal', 'Encargado de todos los asuntos legales de la empresa');
+INSERT INTO employee_department VALUES (1, 'Finanzas', 'Control de gastos de la empresa');
+INSERT INTO employee_department VALUES (2, 'Mantenimiento', 'Vela por el cuidado de las instalaciones');
+INSERT INTO employee_department VALUES (3, 'Informatica', 'Soporte a todos los sistemas de Computo de la empresa');
+INSERT INTO employee_department VALUES (4, 'Personal', 'Gestión del Recurso Humano');
+INSERT INTO employee_department VALUES (5, 'Seguridad', 'Vela por la seguridad de los bienes y personal');
+INSERT INTO employee_department VALUES (6, 'Legal', 'Encargado de todos los asuntos legales de la empresa');
 
-ALTER TABLE employee ADD id_departmet int;
-ALTER TABLE employee ADD FOREIGN KEY(id_departmet) REFERENCES employee_department(id)
+ALTER TABLE employee ADD id_department int;
+ALTER TABLE employee ADD FOREIGN KEY(id_department) REFERENCES employee_department(id)
 
-UPDATE employee SET id_departmet = 3 WHERE id = 1;
-UPDATE employee SET id_departmet = 3 WHERE id = 2;
-UPDATE employee SET id_departmet = 6 WHERE id = 3;
-UPDATE employee SET id_departmet = 2 WHERE id = 4;
+UPDATE employee SET id_department = 3 WHERE id = 1;
+UPDATE employee SET id_department = 3 WHERE id = 2;
+UPDATE employee SET id_department = 6 WHERE id = 3;
+UPDATE employee SET id_department = 2 WHERE id = 4;
 
 -- ...
