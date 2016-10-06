4f4d52f9a29d960a7dd998a4f58c2c0244068ca9
djmolletones <dannymolletones@hotmail.com>
2016-10-06 01:27:50 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,24 +3,21 @@ Modulo para obtener la sumatoria de una lista de numeros
 Author: Danny J. Molletones
 Date: 05/10/2016
 """
-
-
 class CalculatorClass(object):
     """
-    Clase que define un metodo que permite
-    obtenerla sumatoria de una lista de numeros
+    Clases que define un metodo que permite obtener
+    la sumatoria de una lista de numeros
     """
     def sum(self, num_list):
         """
-        Suma el contenido de la lista num_list que es recibida como
+        Suma el contenido de la lista num_list que es recibida como 
         parametro y muestra el resultado por pantalla
         """
+        # your sum code here
         suma = 0
-        cont = 0
-        for cont in num_list:
-            suma = suma + cont
-        print 'La sumatoria es:' + str(suma)
-
+        for i in num_list:
+            suma = suma + i
+        print('La sumatoria es:' + str(suma))
 lista = []
 sumatoria = CalculatorClass()
 cantidad_lista = int(input('Indique la cantidad de numeros a sumar: '))
