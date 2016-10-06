00a539eec3de34b265a8561c6225f80f19eda951
djmolletones <dannymolletones@hotmail.com>
2016-10-06 01:23:39 -0400

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,16 +3,14 @@ Modulo para saber si un numero n es primo o no
 Author: Danny J. Molletones
 Date: 05/10/2016
 """
-
-
 class PrimeClass(object):
     def is_prime(self, num_int):
         """
-        Determina si un numero es considerado
+        Determina si un número es considerado 
         primo al ser divisible por si mismo
         """
         for cont in range(2, num_int):
-            if (num_int % cont) == 0:
+            if (num_int%cont)==0:
                 return False
         return True
 
